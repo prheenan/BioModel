400abad1e1be37d0d269d18f3f70b6d8f85b7939
Your Name <patirck.heenan@colorado.edu>
2016-04-23 00:33:28 -0600

    added in a ModelData function to encapsulate a unit testing set, saving it out as a plot.

--- WormLikeChain/Python/TestExamples/Main_WLC_Fit.py
+++ WormLikeChain/Python/TestExamples/Main_WLC_Fit.py
@@ -10,6 +10,27 @@ sys.path.append(baseDir)
 sys.path.append("../../../")
 import Code.WLC_Fit as WLC_Fit
 
+class ModelData:
+    """
+    Class to keep track of a (generated) unit test data set 
+    """
+    def __init__(self,ext,force,params,noise,name):
+        """
+        Initialize a new model object
+
+        Args:
+            ext: extension, noise-free, preedicted by model
+            force: force, noise free, predicted by model 
+            params: WLC_Fit.WlcParamValues used to generate the object
+            noise: Noise term, to add on (ideally, should match the literature)
+            name: Name of the model
+        """
+        self.ext = ext
+        self.force = force
+        self.params = params
+        self.noise = noise
+        self.name = name
+
 def GetBouichatData():
     """
     Returns samples from data from Figure 1
@@ -34,20 +55,30 @@ web.mit.edu/cortiz/www/3.052/3.052CourseReader/38_BouchiatBiophysicalJ1999.pdf
     # note, by the inset in figure 1 inset / 3 error bars, 2pN is an upper
     # bound on the error we have everywhere
     noiseAmplitude = 2e-12
-    return x,y,params,noiseAmplitude
+    return ModelData(x,y,params,noiseAmplitude,"Bouchiat 1999")
 
-def run():
+def CheckDataObj(DataObj,OutName=None):
     """
-    Runs some unit testing on the WLC fitting
+    Given a ModelData object, checks its parameters match what we would
+    expect, then plots / shows, depending on OutName
+
+    Args:
+        DataObj : instance of ModelData, keeping all the data we want
+        OutName : If not None, saves a plot.
     """
-    x,y,params,noiseAmplitude = GetBouichatData()
+    x = DataObj.ext
+    y = DataObj.force
+    params = DataObj.params
+    noiseAmplitude = DataObj.noise
+    name = DataObj.name
     yPure = y.copy()
     y += noiseAmplitude * (np.random.rand(*y.shape)-0.5)*2
+    print("Fitting Data From {:s}...".format(name))
     # get an extensible and non-extensible model, choose whether to varying L0
     # and Lp
     extensibleFit = WLC_Fit.ExtensibleWlcFit(x,y,VaryL0=True,
                                              VaryLp=True,VaryK0=False)
-    # make sure the parameters match
+    # make sure the parameters match what the model says it should 
     assert extensibleFit.Info.ParamVals.CloseTo(params)
     nonExtensibleFit = WLC_Fit.NonExtensibleWlcFit(x,y,VaryL0=True,
                                                    VaryLp=False)
@@ -57,24 +88,35 @@ def run():
     print(nonExtensibleFit)
     mFit = extensibleFit.Prediction
     mFitNon = nonExtensibleFit.Prediction
-    # plot everything
-    toNm = 1e9
-    toPn = 1e12
-    plt.subplot(2,1,1)
-    plt.plot(x*toNm,yPure*toPn,color='b',label="Data, No Noise")
-    plt.xlabel("Distance (nm)")
-    plt.ylabel("Force (pN)")
-    plt.legend(loc='upper left')
-    plt.title("Extensible WLC better approximates DNA data")
-    plt.subplot(2,1,2)
-    plt.plot(x*toNm,y*toPn,label="Data,With Noise")
-    plt.plot(x*toNm,mFit*toPn,'r-',label="Extensible",linewidth=2.0)
-    plt.plot(x*toNm,mFitNon*toPn,'b--',label="Non Extensible")
-    plt.ylim([min(y*toPn),max(y*toPn)])
-    plt.xlabel("Distance (nm)")
-    plt.ylabel("Force (pN)")
-    plt.legend(loc='upper left')
-    plt.show()
+    if (OutName is not None):
+        # plot everything
+        toNm = 1e9
+        toPn = 1e12
+        fig = plt.figure()
+        plt.subplot(2,1,1)
+        plt.plot(x*toNm,yPure*toPn,color='b',
+                 label="Data, {:s} (No Noise)".format(name))
+        plt.xlabel("Distance (nm)")
+        plt.ylabel("Force (pN)")
+        plt.legend(loc='upper left')
+        plt.title("Extensible WLC better approximates DNA data")
+        plt.subplot(2,1,2)
+        plt.plot(x*toNm,y*toPn,label="Data,With Noise")
+        plt.plot(x*toNm,mFit*toPn,'r-',label="Extensible",linewidth=2.0)
+        plt.plot(x*toNm,mFitNon*toPn,'b--',label="Non Extensible")
+        plt.ylim([min(y*toPn),max(y*toPn)])
+        plt.xlabel("Distance (nm)")
+        plt.ylabel("Force (pN)")
+        plt.legend(loc='upper left')
+        plt.tight_layout()
+        fig.savefig(OutName + ".png")
+    
+def run():
+    """
+    Runs some unit testing on the WLC fitting
+    """
+    DataObj = GetBouichatData()
+    CheckDataObj(DataObj,OutName=DataObj.name)    
 
 if __name__ == "__main__":
     run()
