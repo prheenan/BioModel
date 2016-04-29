2f92be2ca0382b275525769d51274a454d30c347
Your Name <patirck.heenan@colorado.edu>
2016-04-29 16:11:54 -0600

    added in better labelling for the timing, wel boudned for 3 ordered of magnitude

--- WormLikeChain/Python/TestExamples/Timing/Main_WLC_Timing.py
+++ WormLikeChain/Python/TestExamples/Timing/Main_WLC_Timing.py
@@ -17,7 +17,8 @@ import Code.WLC_Fit as WLC_Fit
 from timeit import Timer
 
 def GetSteps(rangeV):
-    numPoints = np.array([1.5e3,2e3,1e4,2e4,5e4,7e4,8e4,1e5,1.5e5,2e5,5e5,1e6])
+    numPoints = np.array([1.5e3,2e3,5e3,1e4,2e4,5e4,7e4,8e4,1e5,1.5e5,2e5,3.5e5,
+                          5e5,1e6])
     steps = rangeV/numPoints
     return steps
 
@@ -51,6 +52,9 @@ def RunTimingTests(limit):
     # over the given literature values
     # note we reverse it so we 'fail fast'
     IndiviudalFuncs = [BullDataTests,BouchiatDataTests]
+    styles = [
+        dict(label="Protein Input",color='r',marker='x'),
+        dict(label="DNA Input",color='b',marker='o')]
     StepsArray = []
     FunctionsArray = []
     for testFunc in IndiviudalFuncs:
@@ -60,26 +64,33 @@ def RunTimingTests(limit):
     InputSize = []
     TimeSize = []
     for Steps,Function in zip(StepsArray,FunctionsArray):
+        tmpTime = []
+        tmpSize = []
         for i,s in enumerate(Steps):
             if (i == limit):
                 break
             obj = Function(s)
             time = TimeFitting(obj)
-            InputSize.append(obj.n)
-            TimeSize.append(time)
+            tmpSize.append(obj.n)
+            tmpTime.append(time)
+        InputSize.append(tmpSize)
+        TimeSize.append(tmpTime)
     # make a graph of the timing results
     sortIdx = np.argsort(InputSize)
-    xSize = np.array(InputSize)[sortIdx] / 1000
-    yTime = np.array(TimeSize)[sortIdx] * 1000
     # array for interpolating the bounds
-    interpX = np.linspace(min(xSize),max(xSize),xSize.size*50)
+    flatX = np.ravel(InputSize) / 1000
+    interpX = np.linspace(min(flatX),max(flatX),flatX.size*50)
     fig = plt.figure()
     ax = plt.subplot(1,1,1)
-    plt.plot(xSize,yTime,'ro',label='Simulated Time')
+    for listSize,listTime,sty in zip(InputSize,TimeSize,styles):
+        n = np.array(listSize)/1000
+        t = np.array(listTime)*1000
+        plt.plot(n,t,**sty)
     plt.plot(interpX,np.log(interpX)*interpX*4,'b--',label="Upper, N * log(N)")
     plt.plot(interpX,np.log(interpX)*interpX/3,'g-',label="Lower, N * log(N)")
+    plt.title("Extensible WLC Runtime is well-bounded by N*Log(N)")
     plt.xlabel("Input Size (Thousands of Points)")
-    plt.ylabel("Time (ms)")
+    plt.ylabel("RunTime (ms)")
     plt.legend(loc='upper left')
     ax.set_xscale('log')
     ax.set_yscale('log')
