switched to a fit and predict-style, with separate functions. seems to be working well now. also fixed beta, evrything has proper units, which helped a lot. normalization defaulting to false, prediction function is the fitting function. for the final prediction, using the prediction function (slightly inefficient)

BellZhurkov/Python/Code/BellZhurkov.py
@@ -48,10 +48,8 @@ def GenBellZurkovFit(Force,Rates,values,vary=None,
     Model = BellZhurkovLogModel
     mVals = BellParamValues(Vary=vary,Bounds=bounds,Values=values)
     Options = FitInfo(FunctionToCall=Model,ParamVals=mVals,
-                      Initialization=Initial)
+                      Initialization=Initial,FunctionToPredict=BellZhurkovModel)
     toRet =  FitMain.Fit(Force,np.log(Rates),Options)
-    # need to exponentiate, since we fitted to the log model
-    toRet.Prediction = np.exp(toRet.Prediction )
     return toRet
 
 

BellZhurkov/Python/TestExamples/Examples/Bell_Examples.py
@@ -23,7 +23,7 @@ def RunWoodsideFigure6():
     """
 
     """
-    GuessDict = dict(beta=1,
+    GuessDict = dict(beta=1/(4.1e-21),
                      k0=60,
                      DeltaX=20e-9,
                      DeltaG=(max(Forces)-min(Forces)))
@@ -34,9 +34,9 @@ def RunWoodsideFigure6():
     xMax=15e-12
     # how much should we interpolate?
     numPredict = (len(Forces)+1)*50
-    xRangePredict = Forces
-    predictFold = infFold.Prediction 
-    predictUnfold = infUnfold.Prediction
+    xRangePredict = np.linspace(xMin,xMax,numPredict)
+    predictFold = infFold.Predict(xRangePredict)
+    predictUnfold = infUnfold.Predict(xRangePredict)
     markerDict = dict(marker='o',
                       markersize=7,
                       linewidth=0,