fixed normalization, so that parameters are also normalized. for FJC, need to interpolate on a grid once the parameter are all fit. may need to change how things are fed into the fit.

FitUtils/Python/FitClasses.py
@@ -7,7 +7,8 @@ import sys
 
 
 from collections import OrderedDict
-MACHINE_EPSILON = np.finfo(float).eps
+import copy
+# abstract base class
 import abc
 
 class BoundsObj:
@@ -483,10 +484,19 @@ class FitReturnInfo:
             we fit with
         Returns: prediction of the y values...
         """
-        params = self.Info.ParamVals.GetValueDict()
+        # get the fit options (for normalization)
         norms = self.Info.FitOptions
+        # get a copy of the parameters
+        paramObj = copy.deepcopy(self.Info.ParamVals)
+        xNorm = norms.NormX
+        yNorm = norms.NormY
+        # normalize the parameters
+        paramObj.NormalizeParams(xNorm,yNorm)
+        # get the values
+        params = paramObj.GetValueDict()
         model = self.Info.FunctionToPredict
-        return model(xVals/norms.NormX,**params)/norms.NormY
+        return model(xVals/xNorm,**params)/yNorm
+    
     def __str__(self):
         return str(self.Info)
 

FreelyJointedChain/Python/TestExamples/Examples/Main_Examples.py
@@ -16,22 +16,27 @@ def PlotSmith1996():
     # fit the data...
     Extension = Data.Extension
     Values = Data.Params.GetValueDict()
-    fit = FreelyJointedChainFit(Extension,Data.ForceWithNoise,Values=Values)
-    extFit = fit.Prediction
+    noiseForce = Data.ForceWithNoise
+    fit = FreelyJointedChainFit(Extension,noiseForce,Values=Values)
+    # now fit on a (relatively) uniform grid from low forst to high force
+    # use a 10x linear grid
+    n = noiseForce.size*10
+    forceGrid = np.linspace(noiseForce[0],noiseForce[-1],n)
+    # get the predictions
+    extGrid = fit.Predict(forceGrid)
     toMicrons = lambda x : x * 1e6
     toPn = lambda x: x * 1e12
     ext = toMicrons(Extension)
+    noiseForcePn = toPn(noiseForce)
     ylim = lambda :  plt.ylim(toPn(Data.ylim))
-    noiseForcePn = toPn(Data.ForceWithNoise)
     # plot the model and fit
     plt.subplot(2,1,1)
     plt.plot(ext,toPn(Data.Force))
-    plt.xlabel("Extension (microns)")
     plt.ylabel("Force (pN)")
     ylim()
     plt.subplot(2,1,2)
-    plt.plot(ext,noiseForcePn,'k',alpha=0.5)
-    plt.plot(toMicrons(extFit),noiseForcePn,'b--')
+    plt.plot(ext,noiseForcePn,'k',alpha=0.3)
+    plt.plot(toMicrons(extGrid),toPn(forceGrid),'b--',linewidth=2)
     plt.xlabel("Extension (microns)")
     plt.ylabel("Force (pN)")
     ylim()