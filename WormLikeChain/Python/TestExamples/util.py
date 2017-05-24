a947ba56cea371bb9c36bfab5bb73a6deb8bcf00
Your Name <patirck.heenan@colorado.edu>
2017-05-24 16:24:27 -0600

    moved most of util stuff to utility class; going to work on timing now to get things faster.

--- WormLikeChain/Python/TestExamples/util.py
+++ WormLikeChain/Python/TestExamples/util.py
@@ -8,12 +8,98 @@ from __future__ import unicode_literals
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
-sys.path.append("../../../../../")
-
 import FitUtil.WormLikeChain.Python.Code.WLC as WLC
-from FitUtil.WormLikeChain.Python.TestExamples import util
-from GeneralUtil.python import PlotUtilities,GenUtilities
+_kbT = 4.11e-21
+_K0 = 1200e-12
+
+class test_object:
+    def __init__(self,name,max_force_N,ext=None,**kwargs):
+        if (ext is None):
+            L0 = dict(**kwargs)["L0"]
+            ext = np.linspace(0,L0,500)
+        self.name = name
+        self.max_force_N = max_force_N
+        self.ext = ext
+        self.param_values = dict(**kwargs)
+
+def GetBullData(StepInNm=0.05):
+    """
+    Returns samples from first unfold of Figure S2.a 
+    http://pubs.acs.org/doi/suppl/10.1021/nn5010588
+
+    Bull, Matthew S., Ruby May A. Sullan, Hongbin Li, and Thomas T. Perkins.
+"Improved Single Molecule Force Spectroscopy Using Micromachined Cantilevers"
+    """
+    # get the extensions used
+    maxXnm = 19
+    nSteps= maxXnm/StepInNm
+    x = np.linspace(0,maxXnm,num=nSteps) * 1e-9
+    L0 = 0.34e-9 * 64
+    """
+    # note: from Supplemental, pp 14 of 
+    Edwards, Devin T., Jaevyn K. Faulk et al 
+    "Optimizing 1-mus-Resolution Single-Molecule Force Spectroscopy..."
+    """
+    Lp = 0.4e-9
+    ParamValues = dict(kbT = 4.11e-21,L0 = L0,
+                       Lp =  Lp,K0 = 1318.e-12)
+    Name = "Bull_2014_FigureS2"
+    noiseN = 6.8e-12
+    expectedMax=80e-12
+    return test_object(name="Bull",max_force_N=expectedMax,ext=x,**ParamValues)
+    
+def GetBouichatData(StepInNm=1):
+    """
+    Returns samples from data from Figure 1
+    From "Estimating the Persistence Length of a Worm-Like Chain Molecule ..."
+    C. Bouchiat, M.D. Wang, et al.
+    Biophysical Journal Volume 76, Issue 1, January 1999, Pages 409-413
+
+web.mit.edu/cortiz/www/3.052/3.052CourseReader/38_BouchiatBiophysicalJ1999.pdf
+    
+    Returns:
+        tuple of <z,F> in SI units
+    """
+    # upper and lower bound is taken from Figure 1, note nm scale
+    maxExtNm = 1335
+    # figure out the number of steps at this interpolation
+    nSteps = int(np.ceil(maxExtNm/StepInNm))
+    # get all the extension values
+    x = np.linspace(start=0,stop=maxExtNm,num=nSteps,endpoint=True) * 1e-9
+    # write down their parameter values, figure 1 inset
+    ParamValues = dict(kbT = 4.11e-21,L0 = 1317.52e-9,
+                       Lp =  40.6e-9,K0 = 1318.e-12)
+    # the expected maximum fitted force is also from figure 1
+    expectedMax = 48e-12
+    Name = "Bouchiat_1999_Figure1"
+    return test_object(name=Name,max_force_N=expectedMax,ext=x,**ParamValues)
+
+
+def get_fitting_parameters_with_noise(ext_pred,force_grid,params_fit,
+                                      noise_amplitude_N,ranges,Ns=10,
+                                      **brute_kwargs):
+    """
+    Gets the fitting parameters after corrupting a known signal with noise
+
+    Args:
+        ext_pred: the x values for the fitting
+        force_grid: the y values for the fitting
+        params_fit: to be passed to wc_contour
+        noise_amplitude_N: the (assumed uniformly normal) noise
+        ranges: for parameters to fit, passed to brute
+        Ns: number of points to use, for brute
+        **brute_kwargs: passed to scipy.optimize.brute
+    Returns:
+        tuple of <fit parameters, predicted y, noisy y>
+    """
+    noise_unitless = (np.random.normal(size=force_grid.size))
+    noise_N = noise_amplitude_N * noise_unitless
+    force_noise = force_grid + noise_N
+    brute_dict = dict(ranges=ranges,Ns=Ns,**brute_kwargs)
+    x0,y = WLC.wlc_contour(separation=ext_pred,force=force_noise,
+                           brute_dict=brute_dict,
+                           **params_fit)
+    return x0,y,force_noise
 
 
 def test_parameter_set(test_obj,debug_plot_base=None,
@@ -69,7 +155,7 @@ def test_parameter_set(test_obj,debug_plot_base=None,
         fit_dict_L0 = dict(params_fit=params_L0_fit,
                            ranges=ranges_L0,
                            **common_kwargs)
-        x0,y,force_noise = util.get_fitting_parameters_with_noise(**fit_dict_L0)
+        x0,y,force_noise = get_fitting_parameters_with_noise(**fit_dict_L0)
         # ensure the error is within the bounds
         L0_relative_error = (abs((x0-L0)/L0))[0]
         assert L0_relative_error < L0_relative_tolerance , \
@@ -83,7 +169,7 @@ def test_parameter_set(test_obj,debug_plot_base=None,
         """
         XXX fit Lp -- right now things are too slow. 
         x0,y,force_noise = \
-                util.get_fitting_parameters_with_noise(**fit_dict_L0_and_Lp)
+                get_fitting_parameters_with_noise(**fit_dict_L0_and_Lp)
         # ensure the error is within the bounds
         L0_relative_error = (abs((x0-L0)/L0))[0]
         assert L0_relative_error < L0_relative_tolerance , \
@@ -114,24 +200,27 @@ def test_parameter_set(test_obj,debug_plot_base=None,
         GenUtilities.ensureDirExists(base_dir)
         PlotUtilities.savefig(fig,base_dir + debug_plot_base + ".png")
 
-def run():
-    """
-    Runs some unit testing on the WLC fitting. 
-    """
-    """
-    # note: from Supplemental, pp 14 of 
-    Edwards, Devin T., Jaevyn K. Faulk et al 
-    "Optimizing 1-mus-Resolution Single-Molecule Force Spectroscopy..."
-    """
-    np.random.seed(42)
-    test_objects = [util.GetBouichatData(),
-                    util.GetBullData(),
-                    # try a few 'fake' common cases
-                    util.ssDNA_example_data(),
-                    util.dsDNA_example_data(),
-                    util.protein_example_data()]
-    for t in test_objects:
-        test_parameter_set(t,debug_plot_base=t.name)
+def ssDNA_example_data():
+    params_ssDNA   = dict(kbT=_kbT,K0=_K0,L0=60e-9,Lp=0.7e-9)
+    to_ret =  test_object(name="ssDNA",
+                          max_force_N=65e-12,
+                          **params_ssDNA)
+    return to_ret
+
+def dsDNA_example_data():
+    params_dsDNA   = dict(kbT=_kbT,K0=_K0,L0=500e-9,Lp=50e-9)
+    to_ret = test_object(name="dsDNA",
+                         max_force_N=65e-12,
+                         **params_dsDNA)
+    return to_ret
+
+def protein_example_data():
+    params_protein = dict(kbT=_kbT,K0=_K0,L0=40e-9,Lp=0.3e-9)
+    to_ret = test_object(name="protein",
+                         max_force_N=100e-12,
+                         **params_protein)
+    return to_ret
+
         
 
 if __name__ == "__main__":
