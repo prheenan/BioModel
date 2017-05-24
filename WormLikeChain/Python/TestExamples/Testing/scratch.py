1baf1e0c53cf07edbaa701e7702a40a9d34a5af5
Your Name <patirck.heenan@colorado.edu>
2017-05-24 11:19:12 -0600

    a functionalized the test

--- WormLikeChain/Python/TestExamples/Testing/scratch.py
+++ WormLikeChain/Python/TestExamples/Testing/scratch.py
@@ -12,6 +12,44 @@ import sys
 sys.path.append("../../../../../")
 
 import FitUtil.WormLikeChain.Python.Code.WLC as WLC
+from GeneralUtil.python import PlotUtilities
+
+def test_parameter_set(param_values,max_force_N,debug_plot_base=None):
+    L0 = param_values["L0"]
+    ext = np.linspace(L0/100,L0,num=1000)
+    force = np.linspace(0,max_force_N)
+    ext_pred,force_grid = WLC.SeventhOrderExtAndForceGrid(F=force,
+                                                          **param_values)
+    force_amplitude_pN = 20e-12
+    # make the uniform noise go from -1 to 1
+    uniform_noise = 2 * (np.random.uniform(size=force_grid.size) - 0.5)
+    noise = force_amplitude_pN * uniform_noise 
+    force_noise = force_grid + noise
+    ranges = [ [max(ext)/5,5*max(ext)]]
+    brute_dict = dict(ranges=ranges,Ns=40)
+    x_grid,y_grid,y_pred = WLC.inverted_wlc(ext=ext_pred,
+                                            force=force_grid,
+                                            **param_values)
+    ParamsFit = dict([  [k,v] for k,v in param_values.items() if k != "L0"])
+    x0,y = WLC.wlc_contour(separation=ext_pred,force=force_noise,
+                             brute_dict=brute_dict,
+                             **ParamsFit)
+    print( (x0-L0)/L0 * 100)
+    if (debug_plot_base is not None):
+        fig = PlotUtilities.figure(figsize=(4,7))
+        ext_pred_plot = ext_pred * 1e9
+        force_noise_plot = force_noise * 1e12
+        y_plot = y * 1e12
+        y_pred_plot = y_pred * 1e12
+        plt.subplot(2,1,1)
+        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3)
+        plt.plot(ext_pred_plot,y_plot,'b--')    
+        PlotUtilities.lazyLabel("","Force (pN)","")
+        plt.subplot(2,1,2)
+        plt.plot(ext_pred_plot,y_pred_plot,'b--')
+        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3)
+        PlotUtilities.lazyLabel("Separation (nm)","Force (pN)","")
+        PlotUtilities.savefig(fig,debug_plot_base + ".png")
 
 def run():
     """
@@ -26,33 +64,9 @@ def run():
     L0 = Lp * 10
     kbT = 4.11e-21
     K0 = 1200e-12
-    ParamValues = dict(kbT =kbT,L0=L0,
-                       Lp =  Lp,K0 = K0)
-    ext = np.linspace(L0/100,L0,num=1000)
-    force = np.linspace(0,50e-12)
-    ext_pred,force_grid = WLC.SeventhOrderExtAndForceGrid(F=force,**ParamValues)
-    force_amplitude_pN = 20e-12
-    # make the uniform noise go from -1 to 1
-    uniform_noise = 2 * (np.random.uniform(size=force_grid.size) - 0.5)
-    noise = force_amplitude_pN * uniform_noise 
-    force_noise = force_grid + noise
-    ranges = [ [max(ext)/5,5*max(ext)]]
-    brute_dict = dict(ranges=ranges,Ns=40)
-    x_grid,y_grid,y_pred = WLC.inverted_wlc(ext=ext_pred,
-                                            force=force_grid,
-                                            **ParamValues)
-    ParamsFit = dict([  [k,v] for k,v in ParamValues.items() if k != "L0"])
-    x0,y = WLC.wlc_contour(separation=ext_pred,force=force_noise,
-                             brute_dict=brute_dict,
-                             **ParamsFit)
-    print( (x0-L0)/L0 * 100)
-    plt.subplot(2,1,1)
-    plt.plot(ext_pred,force_noise,'k-',alpha=0.3)
-    plt.plot(ext_pred,y,'b--')    
-    plt.subplot(2,1,2)
-    plt.plot(ext_pred,y_pred,'b--')
-    plt.plot(ext_pred,force_noise,'k-',alpha=0.3)
-    plt.show()
+    ParamValues = dict(kbT=kbT,L0=L0,Lp=Lp,K0=K0)
+    test_parameter_set(param_values=ParamValues,max_force_N=65e-12,
+                       debug_plot_base="./out")
 
 if __name__ == "__main__":
     run()
