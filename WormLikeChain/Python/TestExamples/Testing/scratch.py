83d0dcd268b3c5e3b098b0eaac2b3756c231214d
Your Name <patirck.heenan@colorado.edu>
2017-05-24 11:42:59 -0600

    removed unused code, added many comments.

--- WormLikeChain/Python/TestExamples/Testing/scratch.py
+++ WormLikeChain/Python/TestExamples/Testing/scratch.py
@@ -21,13 +21,25 @@ class test_object:
 
 
 def get_fitting_parameters_with_noise(ext_pred,force_grid,params_fit,
-                                      noise_amplitude_N):
+                                      noise_amplitude_N,**brute_kwargs):
+    """
+    Gets the fitting parameters after corrupting a known signal with noise
+
+    Args:
+        ext_pred: the x values for the fitting
+        force_grid: the y values for the fitting
+        params_fit: to be passed to wc_contour
+        noise_amplitude_N: the (assumed uniformly normal) noise
+        **brute_kwargs: passed to scipy.optimize.brute
+    Returns:
+        tuple of <fit parameters, predicted y, noisy y>
+    """
     # make the uniform noise go from -1 to 1
     uniform_noise = 2 * (np.random.uniform(size=force_grid.size) - 0.5)
     noise = noise_amplitude_N * uniform_noise 
     force_noise = force_grid + noise
     ranges = [ [np.nanmax(ext_pred)/5,5*np.nanmax(ext_pred)]]
-    brute_dict = dict(ranges=ranges,Ns=40)
+    brute_dict = dict(ranges=ranges,Ns=40,**brute_kwargs)
     x0,y = WLC.wlc_contour(separation=ext_pred,force=force_noise,
                            brute_dict=brute_dict,
                            **params_fit)
@@ -35,7 +47,28 @@ def get_fitting_parameters_with_noise(ext_pred,force_grid,params_fit,
 
 
 def test_parameter_set(param_values,max_force_N,debug_plot_base=None,
-                       L0_relative_tolerance = 1e-2):
+                       L0_relative_tolerance = 1e-2,noise_ampl_N=None):
+    """
+    Tests the given parameter set. throws an error if it failss
+
+    Args:
+        param_values: the expected values of the parameter. Used to generate
+        data
+
+        max_force_N: the maximum force the force-extension curve should go to
+
+        debug_plot_base: if not none, saves plot(s) starting with file path
+     
+        L0_relative_tolerance: maximum acceptable fitting tolerance (e.g. 0.01
+        is 1%)
+    
+        noise_ampl_N: a *list* of noises (in newtons) to use 
+
+    Returns:
+        nothing, throws an error if it fails
+    """
+    if (noise_ampl_N is None):
+        noise_ampl_N = [0,1e-12,5e-12,10e-12,20e-12]
     L0 = param_values["L0"]
     ext = np.linspace(L0/100,L0,num=1000)
     force = np.linspace(0,max_force_N)
@@ -44,16 +77,16 @@ def test_parameter_set(param_values,max_force_N,debug_plot_base=None,
     x_grid,y_grid,y_pred = WLC.inverted_wlc(ext=ext_pred,
                                             force=force_grid,
                                             **param_values)
-    noise_ampl_N = 20e-12
     params_fit = dict([  [k,v] for k,v in param_values.items() if k != "L0"])
-    fit_dict = dict(ext_pred=ext_pred,
-                    force_grid=force_grid,
-                    params_fit=params_fit,
-                    noise_amplitude_N=noise_ampl_N)
-    x0,y,force_noise = get_fitting_parameters_with_noise(**fit_dict)
-    L0_relative_error = abs((x0-L0)/L0)
-    assert L0_relative_error < L0_relative_tolerance , \
-        "Error {:.2g} not in tolerance".format(L0_relative_error)
+    for noise_tmp in noise_ampl_N:
+        fit_dict = dict(ext_pred=ext_pred,
+                        force_grid=force_grid,
+                        params_fit=params_fit,
+                        noise_amplitude_N=noise_tmp)
+        x0,y,force_noise = get_fitting_parameters_with_noise(**fit_dict)
+        L0_relative_error = abs((x0-L0)/L0)
+        assert L0_relative_error < L0_relative_tolerance , \
+            "Error {:.2g} not in tolerance".format(L0_relative_error)
     if (debug_plot_base is not None):
         fig = PlotUtilities.figure(figsize=(4,7))
         ext_pred_plot = ext_pred * 1e9
@@ -61,12 +94,13 @@ def test_parameter_set(param_values,max_force_N,debug_plot_base=None,
         y_plot = y * 1e12
         y_pred_plot = y_pred * 1e12
         plt.subplot(2,1,1)
-        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3)
-        plt.plot(ext_pred_plot,y_plot,'b--')    
+        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3,label="Data")
+        plt.plot(ext_pred_plot,y_plot,'b--',label="Prediction")    
         PlotUtilities.lazyLabel("","Force (pN)","")
         plt.subplot(2,1,2)
-        plt.plot(ext_pred_plot,y_pred_plot,'b--')
-        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3)
+        plt.plot(ext_pred_plot,y_pred_plot,'b--',label="Noiseless Curve")
+        plt.plot(ext_pred_plot,force_noise_plot,'k-',alpha=0.3,
+                 label="Noisy Curve")
         PlotUtilities.lazyLabel("Separation (nm)","Force (pN)","")
         PlotUtilities.savefig(fig,debug_plot_base + ".png")
 
@@ -79,10 +113,6 @@ def run():
     Edwards, Devin T., Jaevyn K. Faulk et al 
     "Optimizing 1-mus-Resolution Single-Molecule Force Spectroscopy..."
     """
-    Lp = 50e-9
-    L0 = Lp * 10
-    kbT = 4.11e-21
-    K0 = 1200e-12
     params_DNA = dict(kbT=4.11e-21,L0=500e-9,Lp=50e-9,K0=1200e-12)
     test_objects = [test_object(max_force_N=65e-12,**params_DNA)]
     for t in test_objects:
