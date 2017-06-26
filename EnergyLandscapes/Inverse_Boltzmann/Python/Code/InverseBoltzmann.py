af933f8cf836fbbe3126d0cbb53e1cf554ff2668
Your Name <patirck.heenan@colorado.edu>
2017-06-26 15:52:05 -0600

    switched to just using sums of gaussian for the pdf. more assertions to check for error conditions. working somewhat better, still need to implement the actual unit test portion...

--- EnergyLandscapes/Inverse_Boltzmann/Python/Code/InverseBoltzmann.py
+++ EnergyLandscapes/Inverse_Boltzmann/Python/Code/InverseBoltzmann.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import sys
 from scipy.signal import fftconvolve
 from scipy.interpolate import interp1d
+import scipy
 
 def inverse_boltzmann(P_q,kT=4.1e-21):
     """
@@ -42,12 +43,36 @@ def deconvolution_iteration(r_0,S_q,P_q,p_k=None):
         p_(k+1) in eq 2 of Woodside, Science,2006, SI, Eq.2 (pp4)
     """
     r = r_0 * (1 - 2*np.abs(p_k-1/2))
-    S_q_convolved_with_p_k = fftconvolve(S_q,p_k,mode='same')
+    S_q_convolved_with_p_k = fftconvolve(p_k,S_q,mode='same')
     p_k_plus_one =  p_k + r * (P_q - S_q_convolved_with_p_k)
     return p_k_plus_one
 
 
-def deconvolve(p_0,n_iters=50,delta_tol=1e-6,return_full=False,**kwargs):
+def f_assert_prob(x,msg):
+    """
+    assets that a given probability density (in units of 1/<something>) is 
+    reasonable. specifically, checks the following:
+
+    (1) Is every element between 0 and 1
+    (2) Is the sum between (0,inf]? Since units aren't given, we can't 
+    assume it will sume to 1 (but it should integrate...)
+
+    Args:
+        x: the probability distribution to check
+        msg: the message to pring on an error
+    Returns:
+        nothing, throws an error if something goes wrong
+    """
+    # make sure every element is between 0 and 1
+    assert ( (x >= 0) & (x <= 1)).all() , msg
+    # since we dont know 'x', we only know the probabilities should
+    # sum to somewhere >0. (e.g. if probability is in units of 
+    # 1/meters, we would need to integrate to get 1)
+    sum_x = sum(x) 
+    assert ( (sum_x > 0)).all() , msg
+    
+
+def deconvolve(p_0,r_0,S_q,P_q,n_iters=50,delta_tol=1e-6,return_full=False):
     """
     deconvolve the probability distrubtion until whichever is first:
 
@@ -57,6 +82,7 @@ def deconvolve(p_0,n_iters=50,delta_tol=1e-6,return_full=False,**kwargs):
 
     Args:
         p_0: initial guess. 
+        r_0, S_q, P_q: see deconvolution_iteration
         n_iters: maximum number of iterations to perform
         delta_tol: if all probabilities change less than delta_tol, we quit
         return_full: if true, returns more detailed information
@@ -66,11 +92,22 @@ def deconvolve(p_0,n_iters=50,delta_tol=1e-6,return_full=False,**kwargs):
         if return_full, tuple of (final probability, list of all probabilities).
         Otherwise, just final probability
     """
+    # check the inputs
+    f_assert_prob(p_0,"Initial guess p_0 not normalized properly (in [0,1])")
+    f_assert_prob(S_q,"Smoothing func S_q not normalized properly (in [0,1])")
+    f_assert_prob(P_q,
+                  "Measured probability P_q not normalized properly (in [0,1])")
+    assert (r_0 > 0) , "Convergence rate must be >0"
+    assert p_0.size == S_q.size , \
+        "Smoothing function (S_q) must be same size as guess (p_0)"
+    assert p_0.size == P_q.size , \
+        "Measured distribution (P_q) must be same size as guess (p_0)"
+    # POST: probabilities should be OK. should be able to run.
     all_probs = [p_0]
     p_k = p_0
     # iteratively update the probability 
     for i in range(n_iters):
-        p_next = deconvolution_iteration(p_k=p_k,**kwargs)
+        p_next = deconvolution_iteration(p_k=p_k,S_q=S_q,P_q=P_q,r_0=r_0)
         error = np.abs(p_next - p_k)
         if return_full:
             # append the current iteration
@@ -79,6 +116,8 @@ def deconvolve(p_0,n_iters=50,delta_tol=1e-6,return_full=False,**kwargs):
              # then the error condition has been reached
              break
         p_k = p_next
+        assert (p_k >= 0).all() , \
+            "Deconvolution error, p_k histogram became negative. Check XXX"
     if (return_full):
         return p_k,all_probs
     else:
@@ -97,11 +136,13 @@ def woodside_2006_smoothing_function(extensions,n=1000):
     # use Woodside, M. T. et al. Science, 2006. SI, Figure S2 for the PSF
     # Since we are convolving, (I think) the average doesnt matter
     woodside_mu = 513.6
-    woodside_fwhm = abs(woodside_mu-512.25)
+    woodside_fwhm = (515.5-512)
+    # see: https://en.wikipedia.org/wiki/Full_width_at_half_maximum
     woodside_stdev = woodside_fwhm/2.355
     n_bins = extensions.size
-    s_psf = np.random.normal(woodside_mu,woodside_stdev,n_bins)
-    S_q, _ = np.histogram(s_psf, n_bins, normed=True)
+    loc = np.mean(extensions)
+    scale = woodside_stdev
+    S_q = scipy.stats.norm.pdf(extensions, loc=loc, scale=scale)
     return S_q
 
 def run():
@@ -116,45 +157,38 @@ def run():
     """
     # use Woodside, M. T. et al. Science, 2006. FIgure S3B for the extension
     # histogram.
-    extension_probability_tuple = [ [0,0],
-                                    [5,0.01],
-                                    [7,0.04],
-                                    [10,0.08],
-                                    [13,0.04],
-                                    [15,0],
-                                    [17,0.01],
-                                    [20,0.05],
-                                    [22,0.12],
-                                    [25,0.05],
-                                    [27,0.01],
-                                    [30,0]]
-    extensions_nm = [e[0] for e in extension_probability_tuple]
-    probability_unnormalized = [ e[1] for e in extension_probability_tuple]
-    f2 = interp1d(extensions_nm,probability_unnormalized, kind='cubic')
-    # get a better plot of probability vs extension
-    n_points = 50
-    extension_grid = np.linspace(min(extensions_nm),max(extensions_nm),
-                                 n_points)
-    probability_grid = f2(extension_grid)
-    # fit a spline to the probability vs extension 
-    probability_integral = np.trapz(y=probability_grid,
-                                    x=extension_grid)
-    probability_normalized = probability_grid/probability_integral
-    probability_normalized = np.maximum(0,probability_normalized)
+    mean_fwhm_nm = [ [10,4], 
+                     [22.5,4.25]]
+    n = 30
+    extensions = np.arange(0,30,step=0.2)
+    probabilities = [scipy.stats.norm.pdf(extensions,
+                                          loc=mu, scale=fwhm/2.355)
+                     for mu,fwhm in mean_fwhm_nm]
+    weights = [1,1.6]
+    full_pdf = np.average(probabilities,axis=0,weights=weights)
+    full_pdf /= np.trapz(y=full_pdf,x=extensions)
+    probability_normalized = np.maximum(0,full_pdf)
     probability_normalized = np.minimum(1,probability_normalized)
     P_q  = probability_normalized
     # XXXX fix extensions = probability_grid.size
-    S_q = woodside_2006_smoothing_function(extensions=probability_grid)
-    r_0 = 1
-    p_0 = np.ones(P_q.size) / P_q.size
+    S_q = woodside_2006_smoothing_function(extensions=extensions)
+    r_0 = 2
+    p_0 = np.ones(P_q.size)
+    p_0 /= np.trapz(y=p_0,x=extensions)
     iterations = 1000
-    p_final,p_list = deconvolve(p_0=p_0,return_full=True,delta_tol=1e-6,
-                                r_0=r_0,S_q=S_q,P_q=P_q)
+    p_final,p_list = deconvolve(p_0=p_0,
+                                r_0=r_0,S_q=S_q,P_q=P_q,n_iters=iterations,
+                                return_full=True,delta_tol=1e-6)
     #... renormalize probability? XXX figure out why this is necessary
-    p_final /= np.trapz(y=p_final,x=extension_grid)
-    extension_grid_plot = extension_grid-min(extension_grid)
-    for p_intermediate in p_list:
-        plt.plot(extension_grid_plot,p_intermediate)
+    p_final /= np.trapz(y=p_final,x=extensions)
+    extension_grid_plot = extensions-min(extensions)
+    # determine the ratios
+    split_point_nm = 15
+    split_idx = np.where(extensions>split_point_nm)[0][0]
+    split_ratios = [np.max(p_final[:split_idx])/np.max(P_q[:split_idx]),
+                    np.max(p_final[split_idx:])/np.max(P_q[split_idx:])]
+    # XXX check that the split ratios are close to what we expect. 
+    plt.plot(extension_grid_plot,S_q,'b:')
     plt.plot(extension_grid_plot,p_final,'g--')
     plt.plot(extension_grid_plot,P_q,linewidth=3,color='r')
     plt.show()
