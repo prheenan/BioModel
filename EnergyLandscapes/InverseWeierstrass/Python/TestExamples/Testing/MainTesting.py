a2a81260f2c848eff2d974d7990ec19319c5ccc4
Your Name <patirck.heenan@colorado.edu>
2017-09-20 19:49:31 -0600

    moved some test utilities to their own

--- EnergyLandscapes/InverseWeierstrass/Python/TestExamples/Testing/MainTesting.py
+++ EnergyLandscapes/InverseWeierstrass/Python/TestExamples/Testing/MainTesting.py
@@ -12,12 +12,8 @@ from scipy.integrate import cumtrapz
 import copy
 from GeneralUtil.python import CheckpointUtilities,GenUtilities,PlotUtilities
 from scipy.interpolate import interp1d
-import Simulation
-
-
-def _f_assert(exp,f,atol=0,rtol=1e-9,**d):
-    value = f(**d)
-    np.testing.assert_allclose(value,exp,atol=atol,rtol=rtol)
+import Simulation,TestUtil
+from TestUtil import _f_assert,HummerData,load_simulated_data
 
 def TestWeighting():
     """
@@ -305,47 +301,6 @@ def assert_noisy_ensemble_correct(fwd,rev):
     n = 50
     _single_direction_assert(fwd,n)
     _single_direction_assert(rev,n)
-
-def get_simulated_ensemble(n,**kw):
-    to_ret = []
-    for _ in range(n):
-        t,q,z,f,p = Simulation.hummer_force_extension_curve(**kw)
-        velocity = p["velocity"]
-        spring_constant = p['k']
-        beta = p['beta']
-        kT = 1/beta
-        good_idx = np.where( (z > 325e-9) & (z < 425e-9))
-        initial_dict = dict(Time=t[good_idx]-t[good_idx][0],
-                            Extension=q[good_idx],
-                            Force=f[good_idx],Velocity=velocity,kT=kT,
-                            SpringConstant=spring_constant)
-        tmp = InverseWeierstrass.FEC_Pulling_Object(**initial_dict)
-        tmp.SetOffsetAndVelocity(z[good_idx][0],tmp.Velocity)
-        to_ret.append(tmp)
-    return to_ret
-
-def load_simulated_data(n,cache_dir="./cache"):
-    """
-    returns: at most n forward and reverse (2*n total) from cache_dir, or
-    re-creates using get_simulated_ensemble
-    """
-    cache_fwd,cache_rev = [cache_dir + s +"/" for s in ["_fwd","_rev"]]
-    GenUtilities.ensureDirExists(cache_fwd)
-    GenUtilities.ensureDirExists(cache_rev)
-    func_fwd = lambda : get_simulated_ensemble(n)
-    func_rev = lambda : get_simulated_ensemble(n,reverse=True)
-    fwd = CheckpointUtilities.multi_load(cache_fwd,func_fwd,limit=n)
-    rev = CheckpointUtilities.multi_load(cache_rev,func_rev,limit=n)
-    return fwd,rev
-
-def HummerData(seed=42,n=10,**kw):
-    np.random.seed(seed)
-    # POST: the ensemble data (without noise) are OK 
-    fwd,rev = load_simulated_data(n=n,**kw)
-    # read in the simulateddata 
-    #assert_noisy_ensemble_correct(fwd,rev)
-    return fwd,rev
-
 def check_iwt_obj(exp,act,**tolerance_kwargs):
     """
     checks that the 'act' iwt object matches the expected 'exp' object. kwargs
