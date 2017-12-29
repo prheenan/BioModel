added in sorting, mean. still not working. derivatives are incorrect.

EnergyLandscapes/InverseWeierstrass/Python/Code/InverseWeierstrass.py
@@ -436,7 +436,7 @@ def get_work_weighted_object(objs,delta_A=0,**kw):
     # POST: j runs over z ('number of bins', except no binning)
     # subtract the mean work
     Wn_raw = np.array([w[-1] for w in works],**array_kw)
-    offset = 0
+    offset = np.mean(works)
     works -= offset
     delta_A = (np.ones(works.shape,**array_kw).T * delta_A).T
     delta_A -= offset
@@ -534,7 +534,8 @@ def free_energy_inverse_weierstrass(unfolding=[],refolding=[]):
         merge(unfold_weighted.f_variance,refold_weighted.f_variance)
     assert weighted_force.size == key.Time.size , "Programming error"
     # all z values should be the same, so just use the key 
-    z = np.sort(key.ZFunc(key))
+    z = key.ZFunc(key)
+    sort_z_idx = np.argsort(z)
     # due to numerical stability problems, may need to exclude some points
     landscape_ge_0 = (weighted_variance > 0)
     n_ge_0 = sum(landscape_ge_0)
@@ -548,7 +549,7 @@ def free_energy_inverse_weierstrass(unfolding=[],refolding=[]):
     where_ok = np.where(landscape_ge_0)[0]
     assert where_ok.size > 0 , "Landscape was zero *everywhere*"
     # POST: landscape is fine everywhere
-    sanit = lambda x: x[where_ok]
+    sanit = lambda x: x[sort_z_idx][where_ok]
     weighted_force = sanit(weighted_force)
     weighted_partition = sanit(weighted_partition)
     weighted_variance = sanit(weighted_variance)

EnergyLandscapes/InverseWeierstrass/Python/TestExamples/Testing/MainTesting.py
@@ -95,14 +95,14 @@ def TestBidirectionalEnsemble():
     kT = 4.1e-21
     Beta = 1/kT
     dA = delta_A_calc
-    landscape_rev = f(refolding=rev_objs)
+    landscape_rev = f(rev_objs)
     landscape_both = f(fwd_objs,rev_objs)
     # add in delta_A to the reverse; should be ~equal to the forward at that 
     # point
     for i,l_tmp in enumerate([landscape_fwd,landscape_both,landscape_rev]):
         check_derivatives(l_tmp)
     plt.plot(landscape_fwd.q,landscape_fwd.G_0)
-    plt.plot(landscape_fwd.q,landscape_rev.G_0)
+    plt.plot(landscape_rev.q,landscape_rev.G_0)
     plt.show()
     np.testing.assert_allclose(landscape_fwd.G_0,landscape_rev.G_0,
                                atol=3*kT,rtol=1e-1)
@@ -128,6 +128,9 @@ def check_hummer_by_ensemble(kT,landscape,landscape_both,f_one_half):
     # for some reason, they offset the energies?... Figure 3A
     energy_offset_kT = 20
     num_bins = landscape.G_0.size
+    plt.plot(landscape.q,landscape.G_0)
+    plt.plot(landscape_both.q,landscape_both.G_0)
+    plt.show()
     landscape_both_kT = (landscape_both.G_0-min(landscape_both.G_0))/kT + \
                         energy_offset_kT
     q_both_rel = landscape_both.q - min(landscape_both.q)
@@ -509,9 +512,17 @@ def check_derivatives(landscape):
     A_z_ddot = landscape.A_z_ddot
     z = landscape.z
     spline_A_z,A_dot_spline_z,A_ddot_spline_z = spline_derivatives(landscape)
+    plt.subplot(2,1,1)
+    plt.plot(A_z)
+    plt.subplot(2,1,2)
+    plt.plot(A_z_weighted)
+    plt.plot(A_dot_spline_z)
+    plt.show()
+
     # the second derivative has higher error...
     relative_loss_1 = _relative_loss(A_dot_spline_z,A_z_weighted)
     relative_loss_2 = _relative_loss(A_ddot_spline_z,A_z_ddot)
+    print(relative_loss_1,relative_loss_2)
     #assert relative_loss_1 < 0.045 , "First derivative loss is too high"
     # second derivative losses are somewhat higher...
     #assert relative_loss_2 < 0.28 , "Second derivative loss is too high"
@@ -603,7 +614,7 @@ def run():
     """
     np.seterr(all='raise')
     np.random.seed(42)
-    #TestWeighting()
+    TestWeighting()
     TestForwardBackward()
     #TestHummer2010()
 